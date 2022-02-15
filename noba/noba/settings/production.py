374b34fdf4331ba119f0d2dcba9e45467d896e2b
Luc5k4yw4lk3r <19277966+Luc5k4yw4lk3r@users.noreply.github.com>
2022-02-15 16:54:14 -0300

    production settings

--- noba/noba/settings/production.py
+++ noba/noba/settings/production.py
@@ -1,6 +1,38 @@
+import os
 from .base import *
 
 DEBUG = False
+SECRETE_KEY = 'q@v$$%!_4$@gn42(ifo17kdgxvusgz0s42i3!c)^j!0(+4_=52'
+ALLOWED_HOST = ['localhost', 'dns.wheteit.is.goingtolive', '134.122.78.176']
+cwd = os.getcwd()
+
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        "NAME": 'nobadb',
+        "USER": 'noba_usr_db',
+        "PASSWORD": '8py1lEQxY+vU^7',
+        "HOST": 'localhost',
+        "PORT": '',
+    }
+}
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://2584eb980d764b3ba6121438ab12a7ce@o1144710.ingest.sentry.io/6208959",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 try:
     from .local import *
